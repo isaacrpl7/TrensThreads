37349ebcff55b4035c3b62902663d435119dfa77
Isaac <isaacrpl7@hotmail.com>
2021-12-01 18:05:35 -0300

    Testando o mutex no trem1 e trem2
    O trem1 espera o trem2 passar, e vice-versa. Faltando descobrir como fazer eles esperarem um pouco antes.

--- trem.cpp
+++ trem.cpp
@@ -1,6 +1,12 @@
 #include "trem.h"
 #include <QtCore>
+#include <mutex>
+#define ANDANDO 0;
+#define ESPERANDO 1;
+#define REGIAO_CRITICA 2;
 
+std::mutex mtx[7];
+int state[7];
 //Construtor
 Trem::Trem(int ID, int x, int y){
     this->ID = ID;
@@ -18,25 +24,42 @@ void Trem::run(){
     while(true){
         switch(ID){
         case 1:     //Trem 1
-            if (y == 30 && x <270)
+            if (y == 30 && x <270){
                 x+=10;
-            else if (x == 270 && y < 150)
-                y+=10;
-            else if (x > 140 && y == 150)
+            }else if (x == 270 && y < 150){
+//                state[0] = ESPERANDO;
+//                if(state[1] != REGIAO_CRITICA)
+//                    state[0] = REGIAO_CRITICA;
+                mtx[0].lock();
+                while(x == 270 && y < 150) {
+                    y+=10;
+                    emit updateGUI(ID, x,y);
+                    msleep(velocidade);
+                }
+                mtx[0].unlock();
+            } else if (x > 140 && y == 150) {
+                //state[0] = ANDANDO;
                 x-=10;
-            else
+            } else
                 y-=10;
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 2: //Trem 2
-            if (y == 30 && x <400)
+            if (y == 30 && x <400){
                 x+=10;
-            else if (x == 400 && y < 150)
+            } else if (x == 400 && y < 150)
                 y+=10;
             else if (x > 270 && y == 150)
                 x-=10;
-            else
-                y-=10;
+            else {
+                mtx[0].lock();
+                while(x == 270 && y > 30) {
+                    y-=10;
+                    emit updateGUI(ID, x,y);
+                    msleep(velocidade);
+                }
+                mtx[0].unlock();
+            }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 3: //Trem 3
