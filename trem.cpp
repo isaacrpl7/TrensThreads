c4807ee89fb8dac62901ecdab94e4e2c3ac591cd
Isaac <isaacrpl7@hotmail.com>
2021-12-01 14:32:16 -0300

    Primeiro commit

---
+++ trem.cpp
@@ -0,0 +1,80 @@
+#include "trem.h"
+#include <QtCore>
+
+//Construtor
+Trem::Trem(int ID, int x, int y){
+    this->ID = ID;
+    this->x = x;
+    this->y = y;
+    velocidade = 100;
+}
+
+void Trem::setVelocidade(int posicao){
+    this->velocidade = posicao;
+}
+
+//Função a ser executada após executar trem->START
+void Trem::run(){
+    while(true){
+        switch(ID){
+        case 1:     //Trem 1
+            if (y == 30 && x <270)
+                x+=10;
+            else if (x == 270 && y < 150)
+                y+=10;
+            else if (x > 140 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 2: //Trem 2
+            if (y == 30 && x <400)
+                x+=10;
+            else if (x == 400 && y < 150)
+                y+=10;
+            else if (x > 270 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 3: //Trem 3
+            if (y == 30 && x <530)
+                x+=10;
+            else if (x == 530 && y < 150)
+                y+=10;
+            else if (x > 400 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 4: //Trem 4
+            if (y == 150 && x <330)
+                x+=10;
+            else if (x == 330 && y < 270)
+                y+=10;
+            else if (x > 200 && y == 270)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 5: //Trem 5
+            if (y == 150 && x <460)
+                x+=10;
+            else if (x == 460 && y < 270)
+                y+=10;
+            else if (x > 330 && y == 270)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        default:
+            break;
+        }
+        msleep(velocidade);
+    }
+}
