48b9edb531b75d8929f2a7bd3bb933b1884cfbbd
Isaac <isaacrpl7@hotmail.com>
2021-12-02 16:44:19 -0300

    Primeira regiao critica adicionada

--- trem.cpp
+++ trem.cpp
@@ -1,6 +1,7 @@
 #include "trem.h"
 #include <QtCore>
 #include <mutex>
+#include <iostream>
 #define ANDANDO 0
 #define ESPERANDO 1
 #define REGIAO_CRITICA 2
@@ -25,20 +26,38 @@ void Trem::run(){
         switch(ID){
         case 1:     //Trem 1
             if (y == 30 && x <270){
-                x+=10;
-            }else if (x == 270 && y < 150){
-//                state[0] = ESPERANDO;
-//                if(state[1] != REGIAO_CRITICA)
-//                    state[0] = REGIAO_CRITICA;
-                mtx[0].lock();
-                while(x == 270 && y < 150) {
-                    y+=10;
-                    emit updateGUI(ID, x,y);
-                    msleep(velocidade);
+                if(state[0] != REGIAO_CRITICA && state[0] != ESPERANDO)
+                    state[0] = ANDANDO;
+                if(state[0] != ESPERANDO){
+                    x+=10;
                 }
-                mtx[0].unlock();
+                if (x > 249 && y == 30 && state[0] != REGIAO_CRITICA){
+                    state[0] = ESPERANDO;
+                    std::cout << "Trem 1 esperando..." << std::endl;
+                }
+                if(state[0] == ESPERANDO) {
+                    mtx[0].lock();
+                    state[0] = REGIAO_CRITICA;
+                    std::cout << "Trem 1 entrou na regiao critica" << std::endl;
+                }
+            }else if (x == 270 && y < 150){
+
+//                mtx[0].lock();
+//                while(x == 270 && y < 150) {
+//                    y+=10;
+//                    emit updateGUI(ID, x,y);
+//                    msleep(velocidade);
+//                }
+//                mtx[0].unlock();
+                if(state[0] == REGIAO_CRITICA){
+                    y+=10;
+                }
             } else if (x > 140 && y == 150) {
-                //state[0] = ANDANDO;
+                if(state[0] == REGIAO_CRITICA){
+                    std::cout << "Trem 1 saiu da regiao critica" << std::endl;
+                    state[0] = ANDANDO;
+                    mtx[0].unlock();
+                }
                 x-=10;
             } else
                 y-=10;
@@ -46,19 +65,39 @@ void Trem::run(){
             break;
         case 2: //Trem 2
             if (y == 30 && x <400){
+                if(state[1] == REGIAO_CRITICA){
+                    mtx[0].unlock();
+                    std::cout << "Trem 2 saiu da regiao critica" << std::endl;
+                }
+
+                state[1] = ANDANDO;
                 x+=10;
             } else if (x == 400 && y < 150)
                 y+=10;
-            else if (x > 270 && y == 150)
-                x-=10;
-            else {
-                mtx[0].lock();
-                while(x == 270 && y > 30) {
-                    y-=10;
-                    emit updateGUI(ID, x,y);
-                    msleep(velocidade);
+            else if (x > 270 && y == 150) {
+                if(state[1] != ESPERANDO)
+                    x-=10;
+                if (x < 291 && y == 150 && state[1] != REGIAO_CRITICA){
+                    state[1] = ESPERANDO;
+                    std::cout << "Trem 2 esperando..." << std::endl;
                 }
-                mtx[0].unlock();
+                if(state[1] == ESPERANDO) {
+                    mtx[0].lock();
+                    std::cout << "Trem 2 entrou na regiao critica" << std::endl;
+                    state[1] = REGIAO_CRITICA;
+                }
+            } else {
+                if(state[1] == REGIAO_CRITICA){
+                    y-=10;
+                }
+
+//                mtx[0].lock();
+//                while(x == 270 && y > 30) {
+//                    y-=10;
+//                    emit updateGUI(ID, x,y);
+//                    msleep(velocidade);
+//                }
+//                mtx[0].unlock();
             }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
